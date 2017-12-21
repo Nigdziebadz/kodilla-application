9d3db96568401e3c1eae688cf043e5c7c4fbf1bf
Nigdziebadz <slowaniepowazne@gmail.com>
2017-12-21 14:54:07 +0100

    Poprawka drzewka

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -1,13 +1,10 @@
-package com.crud.task.repository;
+package com.crud.tasks.repository;
 
 import com.crud.tasks.domain.Task;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
-import javax.transaction.Transactional;
 import java.util.List;
 
-@Transactional
 @Repository
 public interface TaskRepository extends CrudRepository<Task, Long> {
 
--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -1,7 +1,6 @@
 package com.crud.tasks.service;
 
-
-import com.crud.task.repository.TaskRepository;
+import com.crud.tasks.repository.TaskRepository;
 import com.crud.tasks.domain.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
