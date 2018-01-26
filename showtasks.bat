82b8cb8618432018517e30be5297799d4c18f964
“Nigdziebadz <slowaniepowazne@gmail.com>
2018-01-26 09:35:59 +0100

    Deployment per script - crud

--- showtasks.bat
+++ showtasks.bat
@@ -1,9 +1,12 @@
 call runcrud.bat
-if "%ERRORLEVEL%" == "0" goto runchrome
-
+if "%ERRORLEVEL%" == "0" goto waitfordeployment
 echo.
 echo RunCrud.bat have errors, stopping work
 goto fail
+
+:waitfordeployment
+timeout /t 8 /nobreak
+goto runchrome
 
 :runchrome
 start "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://localhost:8080/crud/v1/task/getTasks
