e18af3483a4f4247fe0e6a64fd957b551c18bb79
“Nigdziebadz <slowaniepowazne@gmail.com>
2018-03-08 12:38:23 +0100

    End of 24.2 .3

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -2,6 +2,7 @@ package com.crud.tasks.service;
 
 import com.crud.tasks.config.AdminConfig;
 import com.crud.tasks.config.CompanyInfoConfig;
+import com.crud.tasks.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -19,7 +20,8 @@ public class MailCreatorService {
     private AdminConfig adminConfig;
     @Autowired
     private CompanyInfoConfig companyInfo;
-
+    @Autowired
+    private TaskRepository taskRepository;
     @Autowired
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
@@ -44,6 +46,28 @@ public class MailCreatorService {
         context.setVariable("companyPhone", "tel: " + companyInfo.getCompanyPhone());
         context.setVariable("companyGoal", companyInfo.getCompanyGoal());
         return templateEngine.process("mail/created-trello-card-mail", context);
+    }
+
+    public String buildMailHowManyTasks(String message) {
+
+        long size = taskRepository.count();
+        Context context = new Context();
+        context.setVariable("message", message);
+        context.setVariable("tasks_url", "http://localhost:8888/crud");
+        context.setVariable("button", "Visit website");
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("show_button", false);
+        context.setVariable("is_friend", true);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("howManyTasks", "Currently in databases u got "
+                + ((size == 1) ? size + " task" : size + " tasks"));
+        context.setVariable("preview", "Info about create new card");
+        context.setVariable("goodbye", "See you later :)");
+        context.setVariable("companyName", companyInfo.getCompanyName());
+        context.setVariable("companyMail", "mail: " + companyInfo.getCompanyEmail());
+        context.setVariable("companyPhone", "tel: " + companyInfo.getCompanyPhone());
+        context.setVariable("companyGoal", companyInfo.getCompanyGoal());
+        return templateEngine.process("mail/info-how-many-tasks-in-database", context);
     }
 }
 
